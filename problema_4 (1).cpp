105b5b91f8a43085fda5f4a7db8ea77dce2b7bd2
richisil2005 <166074019+richisil2005@users.noreply.github.com>
2024-10-02 12:39:16 -0500

    Update problema_4 (1).cpp

--- problema_4 (1).cpp
+++ problema_4 (1).cpp
@@ -1,3 +1,8 @@
+//Integrantes:
+//1. Olivares Sarmiento, Javier Ignacio Leon
+//2. Silva Reyes, Santiago Miguel
+//3. Meneses Roncal, Matias Alonso
+
 #include <cmath>
 #include <cstddef>
 #include <cstdio>
